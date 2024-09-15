ced720cf62f166f9335bf67184c6cd3c82a88265
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-15 23:45:41 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,6 +1,7 @@
 <?php
 $addon = rex_addon::get('zip_install');
 
+// Check if the installed version is less than 1.5
 if (rex_version::compare($addon->getVersion(), '1.5', '<')) {
     $tmpPath = rex_path::addon('zip_install', 'tmp');
     
@@ -8,9 +9,10 @@ if (rex_version::compare($addon->getVersion(), '1.5', '<')) {
         // Try to delete the directory
         if (!rex_dir::delete($tmpPath)) {
             // If deletion fails, log a warning
-            rex_logger::logWarning('The temporary directory ' . $tmpPath . ' could not be deleted.');
+            rex_logger::log('The temporary directory ' . $tmpPath . ' could not be deleted.', null, rex_logger::WARNING);
         } else {
-            rex_logger::logInfo('The temporary directory ' . $tmpPath . ' has been successfully deleted.');
+            // Log successful deletion as a notice
+            rex_logger::log('The temporary directory ' . $tmpPath . ' has been successfully deleted.', null, rex_logger::NOTICE);
         }
     }
 }
