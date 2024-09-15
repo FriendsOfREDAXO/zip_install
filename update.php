1efe732ce8cfb83864dfaf3dd379d9490d2b3d27
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-15 23:01:08 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,9 +1,10 @@
 <?php
-    $tmpPath = rex_path::addon('zip_install','tmp');
-    if(!rex_dir::isWritable($tmpPath)) {
-        if(is_dir($tmpPath)) {
-            @chmod($tmpPath, 0755);
-        } else {
-            @mkdir($tmpPath, 0755);
-        }
+$tmpPath = rex_path::addon('zip_install', 'tmp');
+
+if (is_dir($tmpPath)) {
+    // Versuche, das Verzeichnis zu löschen
+    if (!rex_dir::delete($tmpPath)) {
+        // Wenn das Löschen fehlschlägt, gib eine Warnung aus
+        rex_logger::logWarning('Das temporäre Verzeichnis ' . $tmpPath . ' konnte nicht gelöscht werden.');
     }
+}
