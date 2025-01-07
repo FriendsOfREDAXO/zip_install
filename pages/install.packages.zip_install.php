6aaacd9975550c3f3f78fade553cabc7e2ac7121
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-07 11:21:57 +0100

    $csrfField for CSRF-Field

--- pages/install.packages.zip_install.php
+++ pages/install.packages.zip_install.php
@@ -19,13 +19,13 @@ if (rex_request_method() == 'post') {
         echo rex_view::error(rex_i18n::msg('csrf_token_invalid'));
     }
 }
-
+$csrfField = rex_csrf_token::factory('zip_install')->getHiddenField();
 // GitHub search
 $githubContent = '
 <div class="row">
     <div class="col-sm-6">
         <form method="post">
-            ' . rex_csrf_token::factory('zip_install')->getHiddenField() . '
+            ' . $csrfField . '
             <div class="input-group">
                 <span class="input-group-addon"><i class="fa fa-github"></i></span>
                 <input type="text" class="form-control" id="github_user" name="github_user" placeholder="z.B. FriendsOfREDAXO" value="">
@@ -61,7 +61,7 @@ if (isset($repos) && is_array($repos)) {
                 <div class="zip-panel-body">
                     <div class="zip-description">' . rex_escape($repo['description']) . '</div>
                     <form method="post">
-                        ' . rex_csrf_token::factory('zip_install')->getHiddenField() . '
+                        ' . r$csrfField . '
                         <input type="hidden" name="zip_url" value="' . $repo['download_url'] . '">
                         <div class="zip-button-container">
                             <button type="submit" class="btn btn-primary">Installieren</button>
@@ -87,7 +87,7 @@ echo $fragment->parse('core/page/section.php');
 // Upload form
 $uploadContent = '
 <form method="post" enctype="multipart/form-data">
-    ' . rex_csrf_token::factory('zip_install')->getHiddenField() . '
+    ' . $csrfField . '
     <div class="form-group">
         <label for="zip">' . rex_i18n::msg('zip_install_choose_file') . '</label>
         <input type="file" class="form-control" id="zip" name="zip_file" accept=".zip">
@@ -104,7 +104,7 @@ echo $fragment->parse('core/page/section.php');
 // URL form
 $urlContent = '
 <form method="post">
-    ' . rex_csrf_token::factory('zip_install')->getHiddenField() . '
+    ' . $csrfField . '
     <div class="form-group">
         <label for="zip_url">' . rex_i18n::msg('zip_install_url') . '</label>
         <input type="text" class="form-control" id="zip_url" name="zip_url" placeholder="https://github.com/FriendsOfREDAXO/demo_base/archive/refs/heads/main.zip">
