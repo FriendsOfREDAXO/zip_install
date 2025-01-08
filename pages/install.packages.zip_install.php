8f1fbbc35ff60715797248f755020c1974058938
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-08 20:37:32 +0100

    Änderung layout

--- pages/install.packages.zip_install.php
+++ pages/install.packages.zip_install.php
@@ -4,6 +4,22 @@ use FriendsOfRedaxo\ZipInstall\ZipInstall;
 
 $installer = new ZipInstall();
 
+// Define common GitHub users/organizations
+$commonAuthors = [
+    'FriendsOfREDAXO',
+    'yakamara',
+    'tbaddade',
+    'alexplusde',
+    'skerbis',
+    'redaxo',
+    'factorylabs',
+    'aeberhard',
+    'gharlan',
+    'staabm',
+    'christophboecker',
+    'danielsundermeier'
+];
+
 // Handle form submissions
 if (rex_request_method() == 'post') {
     // CSRF protection check
@@ -19,58 +35,71 @@ if (rex_request_method() == 'post') {
         echo rex_view::error(rex_i18n::msg('csrf_token_invalid'));
     }
 }
+
 $csrfField = rex_csrf_token::factory('zip_install')->getHiddenField();
-// GitHub search
+
+// Create datalist HTML
+$datalistHtml = '<datalist id="authors">';
+foreach ($commonAuthors as $author) {
+    $datalistHtml .= '<option value="' . rex_escape($author) . '"></option>';
+}
+$datalistHtml .= '</datalist>';
+
+// Main content with new layout
+$content = '<div class="row">';
+
+// Left column (8 columns) - GitHub search
+$content .= '<div class="col-sm-8">';
+
+// GitHub section
 $githubContent = '
-<div class="row">
-    <div class="col-sm-6">
-        <form method="post">
-            ' . $csrfField . '
-            <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-github"></i></span>
-                <input type="text" class="form-control" id="github_user" name="github_user" placeholder="z.B. FriendsOfREDAXO" value="">
-                <span class="input-group-btn">
-                    <button class="btn btn-primary" type="submit">' . rex_i18n::msg('zip_install_github_search') . '</button>
-                </span>
-            </div>
-        </form>
+<form method="post" class="mb-4">
+    ' . $csrfField . '
+    <div class="input-group input-group-lg">
+        <span class="input-group-addon"><i class="fa fa-github"></i></span>
+        <input type="text" class="form-control" id="github_user" name="github_user" 
+               placeholder="z.B. FriendsOfREDAXO" list="authors" 
+               value="">
+        <span class="input-group-btn">
+            <button class="btn btn-primary" type="submit">' . rex_i18n::msg('zip_install_github_search') . '</button>
+        </span>
     </div>
-    <div class="col-sm-6">
-        <form method="post">
-            ' . rex_csrf_token::factory('zip_install')->getHiddenField() . '
-            <input type="hidden" name="github_user" value="FriendsOfREDAXO">
-            <button type="submit" class="btn btn-default"><i class="fa fa-heart"></i> FriendsOfREDAXO AddOns anzeigen</button>
-        </form>
-    </div>
-</div>';
+    ' . $datalistHtml . '
+</form>';
 
 // Show repos if we have results
 if (isset($repos) && is_array($repos)) {
     $githubContent .= '<div class="row" id="zip_install_repos">';
-    $i = 0;
     foreach ($repos as $repo) {
-        if ($i % 2 === 0 && $i !== 0) {
-            $githubContent .= '</div><div class="row">';
-        }
         $githubContent .= '
         <div class="col-sm-6">
             <div class="zip-panel">
                 <div class="zip-panel-header">
-                    <h4 class="zip-panel-title"><a href="' . $repo['url'] . '" target="_blank">' . rex_escape($repo['name']) . '</a></h4>
+                    <div class="zip-panel-header-content">
+                        <h4 class="zip-panel-title">
+                            <a href="' . $repo['url'] . '" target="_blank">' . rex_escape($repo['name']) . '</a>
+                        </h4>
+                        <form method="post" class="zip-download-form">
+                            ' . $csrfField . '
+                            <input type="hidden" name="zip_url" value="' . $repo['download_url'] . '">
+                            <button type="submit" class="btn btn-primary btn-sm">
+                                <i class="fa fa-download"></i> Installieren
+                            </button>
+                        </form>
+                    </div>
                 </div>
                 <div class="zip-panel-body">
-                    <div class="zip-description">' . rex_escape($repo['description']) . '</div>
-                    <form method="post">
-                        ' . $csrfField . '
-                        <input type="hidden" name="zip_url" value="' . $repo['download_url'] . '">
-                        <div class="zip-button-container">
-                            <button type="submit" class="btn btn-primary">Installieren</button>
+                    <div class="zip-content-wrapper">
+                        <div class="zip-repo-image">
+                            <img src="https://opengraph.githubassets.com/1/' . rex_escape($githubUser) . '/' . rex_escape($repo['name']) . '" 
+                                 alt="' . rex_escape($repo['name']) . '"
+                                 onerror="this.onerror=null; this.src=\'../assets/addons/zip_install/images/repo-placeholder.svg\';">
                         </div>
-                    </form>
+                        <div class="zip-description">' . rex_escape($repo['description']) . '</div>
+                    </div>
                 </div>
             </div>
         </div>';
-        $i++;
     }
     $githubContent .= '</div>';
 } elseif (isset($repos)) {
@@ -79,10 +108,10 @@ if (isset($repos) && is_array($repos)) {
     $githubContent .= '<p class="help-block">' . rex_i18n::msg('zip_install_github_info') . '</p>';
 }
 
-$fragment = new rex_fragment();
-$fragment->setVar('title', 'GitHub AddOns');
-$fragment->setVar('body', $githubContent, false);
-echo $fragment->parse('core/page/section.php');
+$content .= $githubContent . '</div>';
+
+// Right column (4 columns) - Upload and URL forms
+$content .= '<div class="col-sm-4">';
 
 // Upload form
 $uploadContent = '
@@ -99,7 +128,7 @@ $uploadContent = '
 $fragment = new rex_fragment();
 $fragment->setVar('title', rex_i18n::msg('zip_install_file_upload'));
 $fragment->setVar('body', $uploadContent, false);
-echo $fragment->parse('core/page/section.php');
+$content .= $fragment->parse('core/page/section.php');
 
 // URL form
 $urlContent = '
@@ -107,7 +136,8 @@ $urlContent = '
     ' . $csrfField . '
     <div class="form-group">
         <label for="zip_url">' . rex_i18n::msg('zip_install_url') . '</label>
-        <input type="text" class="form-control" id="zip_url" name="zip_url" placeholder="https://github.com/FriendsOfREDAXO/demo_base/archive/refs/heads/main.zip">
+        <input type="text" class="form-control" id="zip_url" name="zip_url" 
+               placeholder="https://github.com/FriendsOfREDAXO/demo_base/archive/refs/heads/main.zip">
     </div>
     <button type="submit" class="btn btn-primary">ZIP herunterladen</button>
 </form>';
@@ -115,4 +145,13 @@ $urlContent = '
 $fragment = new rex_fragment();
 $fragment->setVar('title', 'ZIP URL');
 $fragment->setVar('body', $urlContent, false);
+$content .= $fragment->parse('core/page/section.php');
+
+$content .= '</div>'; // End right column
+$content .= '</div>'; // End row
+
+// Output the full content
+$fragment = new rex_fragment();
+$fragment->setVar('title', 'AddOn Installation');
+$fragment->setVar('body', $content, false);
 echo $fragment->parse('core/page/section.php');
